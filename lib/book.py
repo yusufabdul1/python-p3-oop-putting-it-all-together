9c94dc90a8f826bffe749f92b08b74b5c18a535e
Yusuf Abdul <yusufabdul212112@gmail.com>
2024-12-25 05:56:33 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, pages, author="Unknown", genre="Unknown"):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.genre = genre
+
+    @property
+    def page_count(self):
+        return self.pages
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        self.pages = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, {self.pages} pages, Genre: {self.genre}"
+
+    def is_long(self):
+        return self.pages > 300
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color="Unknown", price=0.0):
+        self.brand = brand
+        self._size = size
+        self.color = color
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    def __str__(self):
+        return f"{self.brand} Shoe, Size: {self.size}, Color: {self.color}, Price: ${self.price}"
+
+    def is_expensive(self):
+        return self.price > 100
